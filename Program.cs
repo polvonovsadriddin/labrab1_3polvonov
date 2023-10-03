5cc451068e3d258ba4bedc1bc91b3ef375aead37
polvonovsadriddin <144240322+polvonovsadriddin@users.noreply.github.com>
2023-10-04 01:36:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿//вариант:3.25
+Console.Write( "Введите n:" ); 
+int n =int.Parse( Console.ReadLine() );
+int m = (((n - (n / 100) * 100)) / 10)+((n / 100) * 100)+((n - (((n/100)*100)+((n % 100) / 10)*10))*10);
+
+Console.WriteLine(m);
